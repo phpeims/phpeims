90c4596fdc2e304691b57f3306621648c43a68e7
deno <inbox@email.com>
2021-05-20 14:15:08 +0800

    add layout

--- resource/view/console/auth/login.php
+++ resource/view/console/auth/login.php
@@ -1,19 +1,5 @@
-<!DOCTYPE html>
-<html lang="zh-Hans">
-<head>
-    <meta charset="utf-8">
-    <title>Login</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/sentsin/layui@v2.6.7/dist/css/layui.css">
-    <link rel="stylesheet" href="{:asset('static/css/app.css')}">
-    <script src="https://cdn.jsdelivr.net/gh/sentsin/layui@v2.6.7/dist/layui.js"></script>
-    <script src="{:asset('static/js/app.js')}"></script>
-    <!--[if lt IE 9]>
-    <script src="https://cdn.jsdelivr.net/npm/html5shiv@3.7.3/dist/html5shiv.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/respond.js@1.4.2/dest/respond.min.js"></script>
-    <![endif]-->
-    {:token_meta()}
-</head>
-<body>
+{layout name="console/layout" /}
+
 <div class="layui-container">
     <div class="layui-row">
         <a href="{:url('/')}">logo</a>
@@ -67,5 +53,4 @@
         });
     });
 </script>
-</body>
-</html>
+
---
+++ resource/view/console/layout.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="zh-Hans">
+<head>
+    <meta charset="utf-8">
+    <title>PHPEIMS</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/sentsin/layui@v2.6.7/dist/css/layui.css">
+    <link rel="stylesheet" href="{:asset('static/css/app.css')}">
+    <script src="https://cdn.jsdelivr.net/gh/sentsin/layui@v2.6.7/dist/layui.js"></script>
+    <script src="{:asset('static/js/app.js')}"></script>
+    <!--[if lt IE 9]>
+    <script src="https://cdn.jsdelivr.net/npm/html5shiv@3.7.3/dist/html5shiv.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/respond.js@1.4.2/dest/respond.min.js"></script>
+    <![endif]-->
+    {:token_meta()}
+</head>
+<body>
+{__CONTENT__}
+</body>
+</html>
