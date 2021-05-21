10ef1745999c62773d1166c1bf324aa5413300ab
deno <inbox@email.com>
2021-05-21 16:45:27 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 wget https://mirrors.aliyun.com/composer/composer.phar -O /usr/local/bin/composer
 chmod a+x /usr/local/bin/composer
 composer config -g repo.packagist composer https://mirrors.aliyun.com/composer/
-composer u -o --no-dev
-chown -R www:www runtime
+composer install -o --no-dev
+chown -R www:www public/storage runtime
